9151ee42792744c748c15983b414e841955e226d
Gary <cheungaryk@gmail.com>
2017-04-18 01:01:30 +0000

    cleaning up

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -7,15 +7,15 @@
 if node['platform_family'] == "rhel"
 	package = "httpd"
 elsif node['platform_family'] == "debian"
-	package = "apache2"
+	package = "apache"
 end
 
-package 'apache2' do
+package 'apache' do
 	package_name package
 	action :install
 end
 
-service 'apache2' do
+service 'apache' do
 	service_name package
 	action [:start, :enable]
 end
